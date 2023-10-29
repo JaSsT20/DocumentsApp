614d7d69144e59113e4b2f08e30320e037bf5a26
Levid <jassontejada@gmail.com>
2023-10-29 11:00:07 -0400

    Adding dagger-hilt

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,9 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.13" apply false
 }
+buildscript {
+    val hilVersion = "2.48"
+    dependencies {
+        classpath("com.google.dagger:hilt-android-gradle-plugin:$hilVersion")
+    }
+}
