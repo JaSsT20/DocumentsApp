7818db1eec431f0a0b17d1e14200b6dcfdf68ac4
Levid <jassontejada@gmail.com>
2023-10-29 09:35:44 -0400

    Adding dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,24 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+
+    //ViewModel
+    val lifeCycleVersion = "2.6.2"
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:$lifeCycleVersion")
+
+    // Para poder usar collectAsStateWithLifeCycle
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:$lifeCycleVersion")
+
+    //navigation
+    val navigationVersion = "2.7.2"
+    implementation("androidx.navigation:navigation-compose:$navigationVersion")
+
+    //retrofit
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.moshi:moshi-kotlin:1.13.0")
+    implementation("com.squareup.retrofit2:converter-moshi:2.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
