dda19ed596ff83a35829f2783af2df66dc77040f
Levid <jassontejada@gmail.com>
2023-10-29 09:46:43 -0400

    Creating DocumentsApi

---
+++ app/src/main/java/com/levid/documentsapp/data/remote/DocumentsApi.kt
@@ -0,0 +1,10 @@
+package com.levid.documentsapp.data.remote
+
+import com.levid.documentsapp.data.remote.dtos.DocumentDto
+import retrofit2.http.GET
+import retrofit2.http.POST
+
+interface DocumentsApi {
+    @GET("/documents")
+    suspend fun getDocuments(): List<DocumentDto>
+}
