5228b6b1614074c91b013afa389bf7e014e8e9e4
Levid <jassontejada@gmail.com>
2023-10-29 12:15:13 -0400

    Calling DocumentScreen

--- app/src/main/java/com/levid/documentsapp/MainActivity.kt
+++ app/src/main/java/com/levid/documentsapp/MainActivity.kt
@@ -10,8 +10,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.levid.documentsapp.data.remote.DocumentsApi
+import com.levid.documentsapp.ui.document.DocumentScreen
 import com.levid.documentsapp.ui.theme.DocumentsAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,25 +26,9 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    DocumentScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DocumentsAppTheme {
-        Greeting("Android")
     }
 }
